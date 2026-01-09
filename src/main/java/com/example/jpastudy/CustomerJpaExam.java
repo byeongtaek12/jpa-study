cb2f79e809f2451370ab03be6dbab90ed1e2fc89
obt <byeongtaek12@naver.com>
2026-01-09 17:40:24 +0900

    study: 순수 JPA 복습 겸 간단 실습 - 준영속 상태시 영속성 컨텍스트에 없기 때문에 select 쿼리 실행되는 것 확인

--- src/main/java/com/example/jpastudy/CustomerJpaExam.java
+++ src/main/java/com/example/jpastudy/CustomerJpaExam.java
@@ -1,14 +1,11 @@
 package com.example.jpastudy;
 
-import java.util.List;
-
 import com.example.jpastudy.entity.Customer;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
-import jakarta.persistence.Query;
 
 public class CustomerJpaExam {
 	public static void main(String[] args) {
@@ -20,12 +17,13 @@ public class CustomerJpaExam {
 
 		try {
 
-			Customer customer = new Customer("ID0004", "Lee"); // 비영속 상태(new)
+			Customer customer = new Customer("ID0005", "Jin"); // 비영속 상태(new)
 			em.persist(customer); // Customer 객체 영속 상태(Managed)
 
-			Query query = em.createQuery("SELECT c FROM Customer c", Customer.class);
-			List<Customer> customers = query.getResultList();
-			System.out.println(customers);
+			em.detach(customer); // Customer 객체 준영속 상태(Detached)
+
+			Customer foundCustomer = em.find(Customer.class, "ID0005");
+			System.out.println(foundCustomer);
 
 			tx.commit();
 		} catch (Exception e) {
