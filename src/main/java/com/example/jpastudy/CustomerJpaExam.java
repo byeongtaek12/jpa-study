86dd21b9d38c6f72516b09720f97b800bfe2ffce
obt <byeongtaek12@naver.com>
2026-01-09 17:19:10 +0900

    study: 순수 JPA 복습 겸 간단 실습 - 비영속 상태 - 영속 상태 - JPQL 사용시 자동 flush 작동 확인 - flush와 commit의 차이

--- src/main/java/com/example/jpastudy/CustomerJpaExam.java
+++ src/main/java/com/example/jpastudy/CustomerJpaExam.java
@@ -1,11 +1,14 @@
 package com.example.jpastudy;
 
+import java.util.List;
+
 import com.example.jpastudy.entity.Customer;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
+import jakarta.persistence.Query;
 
 public class CustomerJpaExam {
 	public static void main(String[] args) {
@@ -17,15 +20,13 @@ public class CustomerJpaExam {
 
 		try {
 
-			// em.persist(Customer.sample());
-			Customer foundCustomer = em.find(Customer.class, "ID0001");
+			Customer customer = new Customer("ID0004", "Lee"); // 비영속 상태(new)
+			em.persist(customer); // Customer 객체 영속 상태(Managed)
 
-			Customer foundCustomer1 = em.find(Customer.class, "ID0001");
-			// foundCustomer.setName("Park");
-			// em.remove(foundCustomer);
-			System.out.println(foundCustomer.toString());
-			System.out.println(foundCustomer1.toString());
-			System.out.println(foundCustomer == foundCustomer1);
+			Query query = em.createQuery("SELECT c FROM Customer c", Customer.class);
+			List<Customer> customers = query.getResultList();
+			System.out.println(customers);
+
 			tx.commit();
 		} catch (Exception e) {
 			tx.rollback();
